2bf85262869429997b969517d9d7ebd50697b96e
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2023-01-17 22:22:11 +0300

    add yaml config

--- os_handler/config/class_config/audio_config.py
+++ os_handler/config/class_config/audio_config.py
@@ -1,9 +1,7 @@
-import pyaudio
-
-
 class AudioConfig:
-    def __init__(self):
-        self.chunk_size: int = 2048
-        self.sample_format: int = pyaudio.paInt16
-        self.channels: int = 1
-        self.frequency: int = 16_000
+    def __init__(self, sample_format: int, chunk_size: int, channels: int,
+                 frequency: int):
+        self.chunk_size = chunk_size
+        self.sample_format = sample_format
+        self.channels = channels
+        self.frequency = frequency
