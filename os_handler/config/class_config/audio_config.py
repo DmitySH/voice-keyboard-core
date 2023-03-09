561e6ba3d8190af495fbc366ba45004445320cd5
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2023-03-09 16:30:27 +0300

    remove one extra folder-level

--- os_handler/config/class_config/audio_config.py
+++
@@ -1,7 +0,0 @@
-class AudioConfig:
-    def __init__(self, sample_format: int, chunk_size: int, channels: int,
-                 frequency: int):
-        self.chunk_size = chunk_size
-        self.sample_format = sample_format
-        self.channels = channels
-        self.frequency = frequency
